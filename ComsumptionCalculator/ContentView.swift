fc17867fc07cd2d6afbfef59fad137d5364c0357
otsuka.amane <otsuka.amane@gmail.com>
2020-05-06 01:01:16 +0900

    Initial Commit

---
+++ ComsumptionCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComsumptionCalculator
+//
+//  Created by 大塚周 on 2020/05/06.
+//  Copyright © 2020 大塚周. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
